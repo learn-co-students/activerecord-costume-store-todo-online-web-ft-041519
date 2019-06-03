b9fe406852e1ca2a2b2a447146bf17ad72153ad9
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-03 22:45:41 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,11 +1,11 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
-    create_table :costumes do |c|
-      c.string :name
-      c.float :price
-      c.string :size 
-      c.string :image_url
-      c.timestamps
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size 
+      t.string :image_url
+      t.timestamps
     end
   end
 end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    create_table :costume_stores do |cs|
-      cs.string :name
-      cs.string :location
-      cs.integer :costume_inventory
-      cs.integer :num_of_employees
-      cs.boolean :still_in_business
-      cs.datetime :opening_time
-      cs.datetime :closing_time
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
